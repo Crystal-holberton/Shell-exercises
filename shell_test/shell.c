c416d1de79d6e1ef35d170cb9ad7521a584aede9
Crystal-holberton <9188@holbertonstudents.com>
2024-12-16 01:12:52 +0000

    arguments

--- shell_test/shell.c
+++ shell_test/shell.c
@@ -8,16 +8,28 @@
 void execute_command(char *command, char *prog_name)
 {
 	char *args[MAX_ARGS];
+	char *token;
+	int i = 0;
 	pid_t pid;
 	int status;
 
-	args[0] = command;
-	args[1] = NULL;
+	token = strtok(command, " ");
+	while (token != NULL && i < MAX_ARGS - 1)
+	{
+		args[i++] = token;
+		token = strtok(NULL, " ");
+	}
+	args[i] = NULL;
+
+	if (args[0] == NULL)
+	{
+		return;
+	}
 
 	pid = fork();
 	if (pid == 0)
 	{
-		if (execve(command, args, environ) == -1)
+		if (execve(args[0], args, environ) == -1)
 		{
 			perror(prog_name);
 			exit(EXIT_FAILURE);
